b17f2516d2e1ecf3e0ba60047cad44cee9abd126
dalvinder47f <sandhuin47@gmail.com>
2019-01-03 13:18:24 +0530

    add all

---
+++ app/src/main/java/com/taxi/driver/AutocompleteFrgActivity.java
@@ -0,0 +1,15 @@
+package com.taxi.driver;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.taxi.driver.R;
+
+public class AutocompleteFrgActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_autocomplete_frg);
+    }
+}
---
+++ app/src/main/java/com/taxi/driver/utils/QuickstartPreferences.java
@@ -0,0 +1,9 @@
+package com.taxi.driver.utils;
+
+/**
+ * Created by bits
+ */
+public class QuickstartPreferences {
+    public static final String SENT_TOKEN_TO_SERVER = "sentTokenToServer";
+    public static final String REGISTRATION_COMPLETE = "registrationComplete";
+}
